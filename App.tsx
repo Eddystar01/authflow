03ebf15bc3f3816ce481d45de7b880dad09298ae
Eddy Dev <olanrewajuolamide.o03@gmail.com>
2026-01-16 04:22:31 +0100

    basic sign in screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Sign in</Text>
+      <TextInput placeholder="Email" style={styles.input} />
+      <TextInput
+      placeholder="Password"
+      secureTextEntry
+      style={styles.input} />
+
+      <Pressable onPress={() => {
+        console.log('pressed');
+      }}
+      style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign up</Text>
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +28,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    gap: 20,
   },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+    borderColor: '#ccc',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  button: {
+    backgroundColor: '#4353fd',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+
 });
